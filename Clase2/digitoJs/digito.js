ca48eba84ebcebe17cd05e4b2bea63802c66445b
CrisCamper <pandeabono2@gmail.com>
2024-02-12 17:27:59 -0500

    nuevos archivos: Clase2/digito.html 	nuevos archivos: Clase2/digitoCss/digito.css 	nuevos archivos: Clase2/digitoJs/digito.js

---
+++ Clase2/digitoJs/digito.js
@@ -0,0 +1,16 @@
+const digitVericator = function(){
+
+    const digitInput = document.getElementById("digit");
+
+    // get digit value
+    const digit = parseInt(digitInput.value);
+
+    // convert number to string
+    modifiedDigit = toString(digit);
+
+    // !invert the string
+    invertDigit = parseInt(modifiedDigit.split('').reverse().join(''));
+
+    // !multiply digits
+    for ()
+}

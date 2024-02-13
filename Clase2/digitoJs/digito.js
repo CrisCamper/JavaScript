81471964bd44662b7d0e177e7407bed65eb77189
CrisCamper <pandeabono2@gmail.com>
2024-02-13 14:30:48 -0500

    modificados:     Clase2/digito.html 	modificados:     Clase2/digitoCss/digito.css 	modificados:     Clase2/digitoJs/digito.js

--- Clase2/digitoJs/digito.js
+++ Clase2/digitoJs/digito.js
@@ -1,16 +1,112 @@
-const digitVericator = function(){
+const iniciar = function () {
+    let lista = ["201012341-3"]
+    localStorage.setItem("data", JSON.stringify(lista));
 
-    const digitInput = document.getElementById("digit");
+}
 
-    // get digit value
-    const digit = parseInt(digitInput.value);
+const listados = function () {
+    var data = JSON.parse(localStorage.getItem("data"));
 
-    // convert number to string
-    modifiedDigit = toString(digit);
+    let boton = document.getElementById("bot3").textContent
 
-    // !invert the string
-    invertDigit = parseInt(modifiedDigit.split('').reverse().join(''));
+    if (boton == "mostrar") {
+        document.getElementById("listado").innerHTML = data;
 
-    // !multiply digits
-    for ()
+        document.getElementById("bot3").innerHTML = "ocultar";
+    }
+    else {
+        document.getElementById("bot3").innerHTML = "mostrar";
+        document.getElementById("listado").innerHTML = "click en mostrar";
+    }
+
+
+}
+
+const repetido = function () {
+    let operando1 = (document.getElementById("operando1").value);
+    var data = JSON.parse(localStorage.getItem("data"));
+
+    let td = data.length;
+    let invertida = "";
+    let multi = "";
+    let modulo = 0;
+    let lista = []
+    lista = data;
+
+
+    invertida = (invertirCadena(operando1));
+
+    multi = (multiplicar(invertida));
+    modulo = multi % 11;
+    resultado = 11 - modulo;
+    console.log(resultado);
+    let pos = lista.includes(operando1 + "-" + resultado)
+    console.log(pos)
+    if (pos == true) {
+        console.log("repetido");
+        alert("el numero esta repetido")
+    }
+    else {
+        console.log("repetido");
+        alert("el numero no esta repetido")
+    }
+
+}
+const verificar = function () {
+    let operando1 = (document.getElementById("operando1").value);
+    var data = JSON.parse(localStorage.getItem("data"));
+
+    let td = data.length;
+    let invertida = "";
+    let multi = "";
+    let modulo = 0;
+    let lista = []
+    lista = data;
+
+
+    invertida = (invertirCadena(operando1));
+
+    multi = (multiplicar(invertida));
+    modulo = multi % 11;
+    resultado = 11 - modulo;
+    console.log(resultado);
+    let pos = lista.includes(operando1 + "-" + resultado)
+    console.log(pos)
+    if (pos == true) {
+        console.log("repetido");
+        alert("el numero esta repetido")
+    }
+    else {
+        document.getElementById("resultado").innerHTML = operando1 + "-" + resultado;
+        lista[td] = operando1 + "-" + resultado;
+        console.log(data);
+
+    }
+    localStorage.setItem("data", JSON.stringify(lista));
+
+}
+
+function invertirCadena(cad) {
+    var separarCadena = cad.split("");
+    var invertirArreglo = separarCadena.reverse();
+    var unirArreglo = invertirArreglo.join("");
+    return unirArreglo;
+}
+function multiplicar(cad) {
+    let n = 0;
+    let j = 1;
+    let suma = 0;
+    var separarCadena = cad.split("");
+    n = separarCadena.length
+    for (let i = 0; i <= n - 1; i++) {
+        j = j + 1;
+        if (j > 7) {
+            j = j - 6;
+        }
+        suma = parseFloat(cad[i]) * j + suma;
+
+    }
+
+
+    return suma;
 }

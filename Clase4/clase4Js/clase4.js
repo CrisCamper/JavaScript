84cc87f3e4840c34daee2f1fbe97b76dacdfb974
CrisCamper <pandeabono2@gmail.com>
2024-02-15 21:42:43 -0500

    modificados:     Clase4/clase4.html	modificados:     Clase4/clase4Css/clase4.css 	modificados:     Clase4/clase4Js/clase4.js

--- Clase4/clase4Js/clase4.js
+++ Clase4/clase4Js/clase4.js
@@ -1,18 +1,22 @@
-// Get the sequence
-const sequenceInput = document.getElementById('sequence');
-const sequence = sequenceInput.value;
-
-// print the sequence
-console.log(sequence);
-
-//get the subsequence
-const subsequenceInput = document.getElementById('subsequence');
-const subsequence = subsequenceInput.value;
-
-// print the subsequence
-console.log(subsequence);
-
 // function of search
-const search = (sequence, subsequence) => {
-    const position = sequence.search(/subsequence/);
-}
+const search = () => {
+    // Get the sequence
+    const sequenceInput = document.getElementById('sequence');
+    const sequence = sequenceInput.value;
+
+    // Get the subsequence
+    const subsequenceInput = document.getElementById('subsequence');
+    const subsequence = subsequenceInput.value;
+
+    // Search for the subsequence in the sequence
+    const index = sequence.indexOf(subsequence);
+    if (index !== -1) {
+        // Highlight the found subsequence
+        const highlightedSequence = sequence.substring(0, index) +
+                                    '<span class="highlight">' + sequence.substring(index, index + subsequence.length) + '</span>' +
+                                    sequence.substring(index + subsequence.length);
+        document.getElementById('result').innerHTML = highlightedSequence;
+    } else {
+        alert(`Subsequence not found`);
+    }
+};

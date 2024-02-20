4719ca0089d6ff2a22c20d6461bb5e7cdb285098
CrisCamper <pandeabono2@gmail.com>
2024-02-20 16:20:35 -0500

    modified:   Clase7/promesas.js

--- Clase7/promesas.js
+++ Clase7/promesas.js
@@ -47,35 +47,36 @@ const facturas = [
     }
 ];
 
-// // Function to get client's name
+// !Function to get client's name
 // const getClient = (id) => {
 //     return new Promise((resolve, reject) => {
 //         const nameClient = clientes.find(c => c.id === id)?.nombre;
 
-//         (nameClient)? resolve("Nombre del cliente: "+nameClient) : reject(`No se encontró el cliente con el id ${id}...` )
+//         (nameClient) ? resolve("Nombre del cliente: " + nameClient) : reject(`No se encontró el cliente con el id ${id}...`)
 //     })
 // }
 
-// // Using the function get client's name
+// //Using the function get client's name
 // getClient(0)
-//     .then((result)=> {console.log(result)})
-//     .catch((error)=> {console.error(error)});
+//     .then((result) => { console.log(result) })
+//     .catch((error) => { console.error(error) });
 
 // !PROMESA DE NUMERO PAR O IMPAR
 
-const parImpar = (num) => {
-    return new Promise((resolve, reject) => {
-        console.warn("Esperando respuesta")
-        if (num % 2 === 0){
-            setTimeout(()=>{
-                resolve(`Este mensaje se ejecutó porque el numero ${num} es par`);
-            }, 2000);
-        }else{
-            setTimeout(()=>{
-                reject(`Este mensaje se ejecutó porque el numero ${num} no es par`);
-            }, 2000);
-        };
-    })
-};
+// const parImpar = (num) => {
 
-parImpar(190).then((positive)=>{console.warn(positive)}).catch((negative)=>{console.error(negative)});
+//     return new Promise((resolve, reject) => {
+//         console.warn("Esperando respuesta")
+//         if (num % 2 === 0) {
+//             setTimeout(() => {
+//                 resolve(`Este mensaje se ejecutó porque el numero ${num} es par`);
+//             }, 2000);
+//         } else {
+//             setTimeout(() => {
+//                 reject(`Este mensaje se ejecutó porque el numero ${num} no es par`);
+//             }, 2000);
+//         };
+//     })
+// };
+
+// parImpar(190).then((positive) => { console.warn(positive) }).catch((negative) => { console.error(negative) });
